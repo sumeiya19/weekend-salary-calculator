affe6920ef5cb010c2dab1cd05150d38dfc59796
Sumeiya Abdi <sumeiya619@gmail.com>
2024-05-05 09:50:04 -0500

    added table to index and complete function of submit button

---
+++ salary.js
@@ -0,0 +1,21 @@
+// Create a form with five inputs
+// Create the tables
+// Styling?
+// Make submit button work
+// Make input from text go into the table (similar to group work)
+    // The footer's total monthly cost should be updated.
+    // The form inputs should be cleared out.
+// Create a delete button 
+// Work on total monthly cost
+// Apply over-budget CSS class
+
+// Create function for submit button
+
+function handleSubmit(event){
+    console.log('handleSubmit () works!');
+    event.preventDefault();
+let firstName = document.getElementById("firstName").value
+console.log(`first name check ${firstName}`);
+let lastName = document.getElementById("lastName").value
+console.log(`last name check ${lastName}`);
+}
